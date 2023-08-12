fc27fcae90538b01426e4913b0cbbcf6f756f6c7
yebbii <ogoo0133@gmail.com>
2023-08-12 15:16:13 +0900

    atoms merge jks and kyb 230812

--- src/components/pages/user/MainPage.js
+++ src/components/pages/user/MainPage.js
@@ -1,19 +1,16 @@
 const MainPage = () => {
-<<<<<<< HEAD
 
 
   return (
     <>
     </>
   )
-=======
   
 
   return (
    <>
    </>
   );
->>>>>>> 04839208d6ac0ba279797e714f85fb5126fa7502
 }
 
 export default MainPage;
